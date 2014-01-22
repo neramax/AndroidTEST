5684220a8cc8b37deb37af39c989097d53139996
Nikola Kujaca <nikola.kujaca@gmail.com>
2014-01-23 00:00:46 +0100

    dodao neke ikonice

--- SplashScreen/gen/com/androidmyway/demo/imagedemo/R.java
+++ SplashScreen/gen/com/androidmyway/demo/imagedemo/R.java
@@ -12,11 +12,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int upload_file=0x7f020001;
+        public static final int upload_file_blue=0x7f020002;
+        public static final int uploadfile=0x7f020003;
+        public static final int uploadfileblue=0x7f020004;
     }
     public static final class id {
         public static final int ImageView=0x7f070001;
-        public static final int camera=0x7f070004;
-        public static final int gallery=0x7f070005;
+        public static final int camera=0x7f070005;
+        public static final int gallery=0x7f070006;
+        public static final int imageView1=0x7f070004;
         public static final int imgcancelbtn=0x7f070003;
         public static final int imguploadbtn=0x7f070002;
         public static final int imguploadtbar=0x7f070000;
@@ -30,6 +35,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int fitId=0x7f040003;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
